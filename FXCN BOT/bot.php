fd7c4c02797e99f05ebc0830b324b2c400a4a3ba
Fxnation <fxcoinfxcn@gmail.com>
2024-10-23 13:26:17 -0700

    Update bot.php

--- FXCN BOT/bot.php
+++ FXCN BOT/bot.php
@@ -10,7 +10,7 @@ if($message == "/start") {
 }
 
 function sendMessage($chat_id, $message) {
-    $apiToken = "7690925310:AAGlIKUhlpjRDl2CsaPL-TTUqF8YB2aQGqw";
+    $apiToken = "<7690925310:AAGlIKUhlpjRDl2CsaPL-TTUqF8YB2aQGqw>";
     $url = "https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chat_id&text=".urlencode($message);
     file_get_contents($url);
 }
